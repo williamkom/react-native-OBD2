52facd867ac41c90b9791600d76d3417531b5ea5
williamkom <willkom57@gmail.com>
2019-03-24 16:31:03 +0100

    Add files via upload

---
+++ Color.js
@@ -0,0 +1,17 @@
+
+/**
+ * Copyright (c) 2016-present JetBridge, LLC.
+ * All rights reserved.
+ *
+ */
+
+'use strict';
+
+module.exports = {
+  BLACK : 'rgba(0, 0, 0, 1)',
+  WHITE : 'rgba(255, 255, 255, 1)',
+  BG_NAVIBAR : 'rgba(85, 114, 128, 1)',
+  DISABLED_COLOR : 'rgba(224, 225, 224, 1)',
+  SETTING_TITLE : 'rgba(255, 82, 82, 1)',
+  SETTING_BORDER_COLOR : 'rgba(220, 220, 220, 1)'
+}
---
+++ PreferenceCategory.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2016-present JetBridge, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  BackAndroid,
+  Dimensions,
+  DeviceEventEmitter,
+  ScrollView
+} from 'react-native';
+
+const Color = require('./Color');
+
+export default class PreferenceCategory extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <Text style={[this.props.style] }> {this.props.text} </Text>
+    );
+  }
+}
+//color:Color.SETTING_TITLE, fontSize: 14, fontWeight: 'bold'
